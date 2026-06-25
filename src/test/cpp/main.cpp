e66dd44fb9780fcdbb7b3ffdcb8c056973d087a3
Tom M <reslip@icloud.com>
2026-06-25 19:12:22 -0400

    update build workflow and add test

--- src/test/cpp/main.cpp
+++ src/test/cpp/main.cpp
@@ -1,8 +1,9 @@
-#include <wpi/wpi/hal/HAL.h>
+#include <wpi/hal/HAL.h>
 
 #include "gtest/gtest.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   HAL_Initialize(500, 0);
   ::testing::InitGoogleTest(&argc, argv);
   int ret = RUN_ALL_TESTS();
